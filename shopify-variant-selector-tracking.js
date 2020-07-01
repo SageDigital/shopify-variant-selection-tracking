4455d624577963f6507035b165f96f9337f2c0a2
Brian Sage <github@sagehome.com>
2020-07-01 13:53:46 -0600

    Ready for Github

--- shopify-variant-selector-tracking.js
+++ shopify-variant-selector-tracking.js
@@ -1,9 +1,8 @@
-// Send User Product and Variant selections to Google Tag Manager
+// Send User Product and Variant selections to dataLayer
 //
 
 // Cache Variant IDs on Script Init
 //
-
 var findVariantById = function (variants, id) {
   return variants.filter(function (o) {
     return o.id.toString() == id;
@@ -13,83 +12,70 @@ var findVariantById = function (variants, id) {
 
 // Send currently selected product to dataLayer
 //
-
 var trackSelectedProduct = function () {
   var selectedVariant = findVariantById(
     ShopifyAnalytics.meta.product.variants,
     ShopifyAnalytics.meta.selectedVariantId
   );
 
-  var eventPayload = {
-    event: 'productVariantSelect',
-    eventAttributes: {
-      action: 'select',
-      category: "productVariant",
-    },
-    ecommerce: {
-      detail: {
-        actionField: {
-          list: 'Product Detail Page',
-        },
-        products: [{
-          name: selectedVariant.name,
-          id: selectedVariant.sku,
-          price: (parseInt(selectedVariant.price) / 100).toString(), // Shopify price presented in cents
-          brand: ShopifyAnalytics.meta.product.vendor,
-          category: ShopifyAnalytics.meta.product.type,
-          variant: selectedVariant.public_title,
-        }],
+  if (selectedVariant) {
+    var eventPayload = {
+      event: 'productVariantSelect',
+      eventAttributes: {
+        action: 'select',
+        category: "productVariant",
       },
-    },
-  };
+      ecommerce: {
+        detail: {
+          actionField: {
+            list: 'Product Detail Page',
+          },
+          products: [{
+            name: selectedVariant.name,
+            id: selectedVariant.id.toString(),
+            sku: selectedVariant.sku,
+            price: (parseInt(selectedVariant.price) / 100).toString(),
+            brand: ShopifyAnalytics.meta.product.vendor,
+            category: ShopifyAnalytics.meta.product.type,
+            variant: selectedVariant.public_title,
+          }],
+        },
+      },
+    };
 
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push(eventPayload);
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push(eventPayload);
+  }
 };
 
 
 // Compare current Variant selection to previous variant selection
 //
-
 var compareToCurrentSelection = function () {
-  window.currentSelection = findVariantById(
-    ShopifyAnalytics.meta.product.variants,
-    ShopifyAnalytics.meta.selectedVariantId
-  );
+  window.currentSelection = ShopifyAnalytics.meta.selectedVariantId;
 
-  if (window.cachedSelection.id !== window.currentSelection.id) {
-    // console.log('UPDATED!', window.currentSelection);
+  if (window.cachedSelection !== window.currentSelection) {
     trackSelectedProduct();
-    window.cachedSelection = findVariantById(
-      ShopifyAnalytics.meta.product.variants,
-      ShopifyAnalytics.meta.selectedVariantId
-    );
   }
+
+  window.cachedSelection = ShopifyAnalytics.meta.selectedVariantId;
 };
 
 
 // Start checking selection
 //
-
-var init = function () {
+var initVariantSelectorTracking = function () {
   // Check for change every 300ms
-  window.cachedSelection = findVariantById(
-    ShopifyAnalytics.meta.product.variants,
-    ShopifyAnalytics.meta.selectedVariantId
-  );
-  window.currentSelection = findVariantById(
-    ShopifyAnalytics.meta.product.variants,
-    ShopifyAnalytics.meta.selectedVariantId
-  );
+  window.cachedSelection = ShopifyAnalytics.meta.selectedVariantId;
+  window.currentSelection = ShopifyAnalytics.meta.selectedVariantId;
 
   window.trackingCheckInterval = window.setInterval(compareToCurrentSelection, 200);
 };
 
 
-// If tracking is ready
+// Init when tracking is ready
 //
-
-var readyCheck = function () {
+var variantSelectorReadyCheck = function () {
   if (
     'ShopifyAnalytics' in window &&
     'meta' in ShopifyAnalytics &&
@@ -97,10 +83,9 @@ var readyCheck = function () {
     'product' in ShopifyAnalytics.meta &&
     'variants' in ShopifyAnalytics.meta.product
   ) {
-    init();
+    initVariantSelectorTracking();
   } else {
-    window.trackingReadyTimeout = window.setTimeout(readyCheck, 200);
+    window.trackingReadyTimeout = window.setTimeout(variantSelectorReadyCheck, 200);
   }
 };
-
-window.trackingReadyTimeout = window.setTimeout(readyCheck, 200);
+window.trackingReadyTimeout = window.setTimeout(variantSelectorReadyCheck, 200);
